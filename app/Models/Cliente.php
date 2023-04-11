1cced741f91dcb5b1acb7fd9cd7e63eb2f27e95b
Ezequiel Zucoli <ezequielzucoli@hotmail.com>
2023-04-11 16:47:40 -0300

    Factory y modelos faltantes
    Me había olvidado de los Factory y de paso cree los otros modelos

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -5,15 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Vehiculo extends Model
+class Cliente extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'id',
-        'modelo',
-        'patente',
-        'modelo',
-        'precio',
+        'name',
+        'lastname',
     ];
 }
--- app/Models/Comentario.php
+++ app/Models/Comentario.php
@@ -5,15 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Vehiculo extends Model
+class Comentario extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'id',
-        'modelo',
-        'patente',
-        'modelo',
-        'precio',
+        'puntuacion',
     ];
 }
