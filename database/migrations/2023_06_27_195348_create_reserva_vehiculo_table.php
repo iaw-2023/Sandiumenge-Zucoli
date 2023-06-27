25682ed0454a7a23e076befa4569027819a0706f
Ezequiel Zucoli <ezequielzucoli@hotmail.com>
2023-06-27 18:48:57 -0300

    Update 2023_06_27_195348_create_reserva_vehiculo_table.php

--- database/migrations/2023_06_27_195348_create_reserva_vehiculo_table.php
+++ database/migrations/2023_06_27_195348_create_reserva_vehiculo_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReservaVehiculoTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -22,5 +22,5 @@ class CreateReservaVehiculoTable extends Migration
     {
         Schema::dropIfExists('reserva_vehiculo');
     }
-}
+};
 
